06f39370e4c3ed061dde73af0a885427ffbef8d1
seis <seis@itu.dk>
2019-10-21 16:04:24 +0200

    Initial commit

---
+++ src/main/java/com/example/devtools/dao/PersonDao.java
@@ -0,0 +1,23 @@
+package com.example.devtools.dao;
+
+import com.example.devtools.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        return insertPerson(UUID.randomUUID(), person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+}
